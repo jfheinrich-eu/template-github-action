b7681158b52ceaf97b16efc6621544151d8b9498
Joerg Heinrich <joerg@jfheinrich.eu>
2025-05-02 07:15:52 +0200

    build: configure development environment
    - vscode
      - Dev Container
      - pyenv python version set to 3.12.10
      - vscode configuration

--- .devcontainer/postAttachCommands.sh
+++ .devcontainer/postAttachCommands.sh
@@ -2,7 +2,7 @@
 
 (gh auth status || gh auth login) && gh extension install https://github.com/nektos/gh-act
 
-pip3 install --user pipreqs pytest pytest-cov flake8 auto8
-pip3 install --user -r requirements.txt
+pip3 install poetry
+poetry install --with test
 
 npm ci
